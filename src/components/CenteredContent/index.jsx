fb19e227cb597f37a707a98cc17eddbe2e5c0f62
Joao Lopes <joaorfclopes@gmail.com>
2020-08-05 11:47:19 +0100

    CenteredContent component fixed

--- src/components/CenteredContent/index.jsx
+++ src/components/CenteredContent/index.jsx
@@ -5,7 +5,9 @@ export default class CenteredContent extends React.Component {
   render = () => {
     return (
       <div className="centered-content-container">
-        <div className="centered-content">{this.props.content}</div>
+        <div className="centered-content-wrapper">
+          <div className="centered-content">{this.props.content}</div>
+        </div>
       </div>
     );
   };
